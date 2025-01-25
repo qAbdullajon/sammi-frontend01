8b447d7bc438613af8d3508ac408493ed670256a
qAbdullajon <azikmelor7705@gmail.com>
2025-01-25 17:23:20 +0500

    Asas

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "https://sammi-backend01.onrender.com";
+export const API_URL = "http://localhost:8080";
 
 const $axios = axios.create({
   withCredentials: true,
