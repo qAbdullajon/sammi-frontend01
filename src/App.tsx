c6fe8f827eb878c3b262187c15442178bf3b145b
qAbdullajon <azikmelor7705@gmail.com>
2025-01-25 23:55:43 +0500

    qwqw

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import Home from "./pages/home";
 import Auth from "./pages/auth";
 import Navbar from "./components/shared/navbar";
 import { toast } from "sonner";
+import { $axios } from "./http";
 import { authStore } from "./store/auth.store";
 import { useEffect } from "react";
 import RecoveryAccount from "./pages/recovery-account";
-import { $axios } from "./http";
 
 function App() {
   const { setIsAuth, setLoading, setUser } = authStore();
@@ -36,6 +36,7 @@ function App() {
   return (
     <>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
