b70c5e0c280dda5e3dc23f3acfe8c37a1059005d
qAbdullajon <azikmelor7705@gmail.com>
2025-01-25 18:34:42 +0500

    Asas

--- src/http/api.ts
+++ src/http/api.ts
@@ -13,22 +13,20 @@ $api.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
 
-    if (error.response.status === 401 && error.config && !error.config._isRetry) {
+    if (error.response?.status === 401 && !originalRequest._isRetry) {
       originalRequest._isRetry = true;
 
       try {
         const { data } = await $axios.get("/auth/refresh");
-        console.log(data);
         localStorage.setItem("accessToken", data.accessToken);
+
         originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
         return $api.request(originalRequest);
       } catch (err) {
-        console.log(err);
-        console.log("Not authorized:");
+        console.log("Not authorized:", err);
       }
     }
 
-    console.log(error);
     throw error;
   }
 );
