0a1e91d8ad61439b2bbc2f4b959fb7b4cfa7d7a8
qAbdullajon <azikmelor7705@gmail.com>
2025-01-25 21:30:46 +0500

    qwqw

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 const API_URL = "https://sammi-backend01.onrender.com";
+axios.defaults.withCredentials = true;
+
 const $axios = axios.create({
   withCredentials: true,
   baseURL: `${API_URL}/api`,
